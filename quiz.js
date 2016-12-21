a4f6bc262ec120ad2ac305a37aed3b731e8847dd
j-west <westja89@gmail.com>
2016-12-20 21:34:48 -0600

    Add logic to assign the value of the option selected to a variable each time an option is selected. Then use those variables to create a new type of robot

--- quiz.js
+++ quiz.js
@@ -1,2 +1,27 @@
-var robot1 = new BattleDome.Fighters.Robot();
-var robot2 = new BattleDome.Fighters.Robot();
+// var robot1 = new BattleDome.Fighters.Robot();
+// var robot2 = new BattleDome.Fighters.Robot();
+  var robot1;
+  var robot2;
+var selectedR1;
+var selectedR2;
+
+$("#toBattle").click((e) => {
+
+   robot1 = new BattleDome.Fighters[selectedR1];
+   robot2 = new BattleDome.Fighters[selectedR2];
+
+   console.log(robot1);
+   console.log(robot2);
+});
+
+
+
+  $("#selection1").change((e) => {
+    selectedR1 = e.target.value;
+    console.log(selectedR1);
+  });
+
+    $("#selection2").change((e) => {
+      selectedR2 = e.target.value;
+      console.log(selectedR2);
+    });
